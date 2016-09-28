28a297172c255a85a12c5c521f661b8ab241dccc
RinaSakai <sakai@calil.jp>
2016-09-28 11:48:51 +0900

    coffeescript関係削除

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,15 +1,9 @@
 var gulp = require('gulp');
 var coffee = require('gulp-coffee');
-var gutil = require('gulp-util');
 var exec = require('child_process').exec;
 var mocha = require('gulp-mocha');
 
-gulp.task('default', function () {
-    return gulp.src('kanikama.coffee')
-        .pipe(coffee({bare: true}).on('error', gutil.log))
-        .pipe(gulp.dest('./')
-    );
-});
+gulp.task('default', ['watch']);
 
 gulp.task('test', ['coverage'],function () {
     return gulp.src('test/test.js', {read: false})
@@ -23,13 +17,6 @@ gulp.task('coverage',function () {
     });
 });
 
-gulp.task('run', function () {
-    return exec('coffee kanikama.coffee', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-    });
-});
-
 gulp.task('watch', function(){
-    return gulp.watch(['./kanikama.coffee','./test/test.coffee'], ['test']);
+    return gulp.watch(['./kanikama.js','./test/test.js'], ['test']);
 });
